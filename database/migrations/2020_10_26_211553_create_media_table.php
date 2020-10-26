b82502e43fcd509d17de3daa76be2619b730c820
Padz <benpadz08@gmail.com>
2020-10-27 06:19:13 +0800

    created media table

---
+++ database/migrations/2020_10_26_211553_create_media_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /** types */
+        //1 - vimeo
+        //2 - jitsi
+        Schema::create('media', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('schedule_id');
+            $table->integer('type');
+            $table->integer('views')->default(0);
+            $table->string('url');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
---
+++ database/seeds/MediaSeeder.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MediaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $media = [
+            [
+                'schedule_id => '1,
+                'type' => 1,
+                'url' => 'https://vimeo.com/471151437',
+            ],
+            [
+                'schedule_id' => 2,
+                'type' => 2
+                'url' => 'https://meet.jit.si/day-1-PCP-OPENING-CEREMONY-2020'
+            ],
+            [
+                'schedule_id' => 3,
+                'type' => 2
+                'url' => 'https://meet.jit.si/day-1-PCP-OPENING-CEREMONY-2020'
+            ],
+            [
+                'schedule_id' => 4,
+                'type' => 2
+                'url' => 'https://meet.jit.si/day-1-PCP-OPENING-CEREMONY-2020'
+            ],
+            ,
+            [
+                'schedule_id' => 5,
+                'type' => 2
+                'url' => 'https://meet.jit.si/day-1-PCP-OPENING-CEREMONY-2020'
+            ],
+            ,
+            [
+                'schedule_id' => 6,
+                'type' => 2
+                'url' => 'https://meet.jit.si/day-1-PCP-OPENING-CEREMONY-2020'
+            ],
+            ,
+            [
+                'schedule_id' => 7,
+                'type' => 2
+                'url' => 'https://meet.jit.si/day-1-PCP-OPENING-CEREMONY-2020'
+            ],
+            [
+                'schedule_id' => 8,
+                'type' => 2
+                'url' => 'https://meet.jit.si/day-1-PCP-OPENING-CEREMONY-2020'
+            ],
+            [
+                'schedule_id' => 9,
+                'type' => 2
+                'url' => 'https://meet.jit.si/day-1-PCP-OPENING-CEREMONY-2020'
+            ],
+            [
+                'schedule_id' => 10,
+                'type' => 2
+                'url' => 'https://meet.jit.si/day-1-PCP-OPENING-CEREMONY-2020'
+            ],
+            
+        ]
+    }
+}
