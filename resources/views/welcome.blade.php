faa4ce90e50b17508d0e43352a6486d85b60daea
Padz <benpadz08@gmail.com>
2020-11-12 11:44:53 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,27 @@
-@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'home', 'title' => __('Material Dashboard')])
+@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'home', 'title' => __('PCP Ilocos-Abra Chapter')])
 
 @section('content')
-<div class="container-fluid" style="height: auto;">
-  <div class="row justify-content-center">
-      <div class="col-12">
-        
+<div class="container" style="height: auto;">
+  <div class="row">
+      <div class="col-md-8">
+        <div class="card" style="width: 100%;">
+          <img class="card-img-top" src="https://images.unsplash.com/photo-1517303650219-83c8b1788c4c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd4c162d27ea317ff8c67255e955e3c8&auto=format&fit=crop&w=2691&q=80" rel="nofollow" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card card-nav-tabs" style="width: 20rem;">
+          <div class="card-header card-header-danger">
+            Featured  
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+        </div>
       </div>
   </div>
 </div>
