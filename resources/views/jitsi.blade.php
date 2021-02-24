aa7b51fed6f28855864a3dd73a72ba691478a2c6
Padz <benpadz08@gmail.com>
2021-02-24 23:14:27 +0800

    Update jitsi.blade.php

--- resources/views/jitsi.blade.php
+++ resources/views/jitsi.blade.php
@@ -2,14 +2,25 @@
 <script>
     var domain = "meet.jit.si";
     var options = {
-        roomName: "{{ $media->url }}",
+        //roomName: "{{ $media->url }}",
+        roomName: "adasdasdq3421",
         parentNode: document.querySelector('#meet'),
         configOverwrite: {
             prejoinPageEnabled: false,
+            disableAudioLevels: true,
+            disableAP: true,
+            disableAEC: true,
+            disableAGC: true,
+            disableNS: true,
+            disableHPF: true,
         },
         interfaceConfigOverwrite: {
             // filmStripOnly: true
+            SET_FILMSTRIP_ENABLED: false,
             HIDE_INVITE_MORE_HEADER: true,
+            DISABLE_FOCUS_INDICATOR: true,
+            DISABLE_DOMINANT_SPEAKER_INDICATOR: true,
+            DISABLE_VIDEO_BACKGROUND: true,
             SHOW_CHROME_EXTENSION_BANNER: false,
             SHOW_JITSI_WATERMARK: false,
             TOOLBAR_BUTTONS: [
